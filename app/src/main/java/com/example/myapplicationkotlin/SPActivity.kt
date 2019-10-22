d20ec5ab67d336270ca07d835e6edd2b3fcf3d4b
Lev <duffyduck451@gmail.com>
2019-10-22 19:49:49 +0200

    sliders work, font added

--- app/src/main/java/com/example/myapplicationkotlin/SPActivity.kt
+++ app/src/main/java/com/example/myapplicationkotlin/SPActivity.kt
@@ -6,9 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.widget.SeekBar
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_focus.*
+import kotlinx.android.synthetic.main.activity_focus.focus_button
+import kotlinx.android.synthetic.main.activity_focus.meditation_button
+import kotlinx.android.synthetic.main.activity_focus.s_p_button
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_s_p.*
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -32,5 +37,42 @@ class SPActivity : AppCompatActivity() {
             startActivity(Intent(this@SPActivity,MeditationActivity::class.java))
         }
 
+        seekBar_duration.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                // Display the current progress of SeekBar
+                number_block_duration.text = "$i"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+                Toast.makeText(applicationContext,"Set block duration in min",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+                Toast.makeText(applicationContext,"Duration was successfully set",Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        seekBar_blocks.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                // Display the current progress of SeekBar
+                number_of_focus_blocks.text = "$i"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+                Toast.makeText(applicationContext,"Set number of blocks",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+                Toast.makeText(applicationContext,"Number of blocks was successfully set",Toast.LENGTH_SHORT).show()
+            }
+        })
+
     }
+
 }
